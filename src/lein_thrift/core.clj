e7f177d75b8ade843ec7ae35a1c9c380c8a27359
Kurt Harriger <kurtharriger@gmial.com>
2012-01-07 11:47:16 -0700

    delete lein generated file

--- src/lein_thrift/core.clj
+++
@@ -1,1 +0,0 @@
-(ns lein-thrift.core)
--- test/lein_thrift/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns lein-thrift.test.core
-  (:use [lein-thrift.core])
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))

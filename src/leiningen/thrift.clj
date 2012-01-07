98c07907011d4e53d42bd055c2dd6483d7f7b53e
Kurt Harriger <kurtharriger@gmial.com>
2012-01-07 11:59:31 -0700

    clean thrift-java dir

--- src/leiningen/thrift.clj
+++ src/leiningen/thrift.clj
@@ -9,7 +9,9 @@
         java-dir (:thrift-java-path project "src/thrift-java")]
     (when (and (fs/exists? src-dir))
       (when-let [src-files (seq (fs/list-dir src-dir))]
-        (when-not (fs/exists? java-dir) (fs/mkdir java-dir))
+        (when (fs/exists? java-dir)
+          (fs/delete java-dir))
+        (fs/mkdir java-dir)
         (doseq [src src-files]
           (sh "thrift" "-r" "--gen" "java:beans,hashcode" "-out" java-dir (str src-dir "/" src))
           (println (str "Generated source for " src)))
